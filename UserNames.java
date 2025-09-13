7569c5d34036852db70676855325ff589f7738da
Maynard Peralta <maynperalta@gmail.com>
2025-09-12 21:45:36 -0600

    Option 2 commit.

--- UserNames.java
+++ UserNames.java
@@ -2,17 +2,16 @@
 public class UserNames {
 
 	public static void main(String[] args) {
-		String userFirstName = "Michael";
-		String userLastName = "Smith";
-		String userAddress = "123 Main St.";
-		String userCity = "Anytown";
-		String userState = "Colorado";
-		int userZip = 81234;
+		String restaurantName = "Chianti Ristorante Italiano";
+		String restaurantAddress = "5121 S. Yosemite St.";
+		String restaurantCity = "Greenwood Village";
+		String restaurantState = "Colorado";
+		int restaurantZip = 80111;
 		
-		System.out.println("Fictional character:");
-		System.out.println(userLastName + ", " + userFirstName);
-		System.out.println(userAddress);
-		System.out.println(userCity + ", " + userState + " " + userZip);
+		System.out.println("For fine Italian dining visit:");
+		System.out.println(restaurantName);
+		System.out.println(restaurantAddress);
+		System.out.println(restaurantCity + ", " + restaurantState + ", " + restaurantZip);
 				
 	}
 
