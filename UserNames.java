dd9bcfb93beb2e0bfd871a65da35d000bc2e820d
Maynard Peralta <maynperalta@gmail.com>
2025-09-12 19:14:41 -0600

    Second commit

--- UserNames.java
+++ UserNames.java
@@ -2,8 +2,18 @@
 public class UserNames {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("Hello, world!!!");
+		String userFirstName = "Michael";
+		String userLastName = "Smith";
+		String userAddress = "123 Main St.";
+		String userCity = "Anytown";
+		String userState = "Colorado";
+		int userZip = 81234;
+		
+		System.out.println("Fictional character:");
+		System.out.println(userLastName + ", " + userFirstName);
+		System.out.println(userAddress);
+		System.out.println(userCity + ", " + userState + " " + userZip);
+				
 	}
 
 }
